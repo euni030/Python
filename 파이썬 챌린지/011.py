bed2374bf7a1cdda4e67bfb98cd257e32d189ef2
euni030 <space_of_record@naver.com>
2023-02-25 19:01:34 +0900

    기초

---
+++ 파이썬 챌린지/011.py
@@ -0,0 +1,5 @@
+largernumber=int(input("Enter the number over 100:"))
+smallernumber=int(input("Enter the number less 10:"))
+
+answer=largernumber/smallernumber
+print(smallernumber,"goes into",largernumber, answer,"times")
