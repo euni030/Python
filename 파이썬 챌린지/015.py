2fd30905495164e505c8a97d95fa976b573ffea6
euni030 <space_of_record@naver.com>
2023-02-25 20:13:41 +0900

    if문

---
+++ 파이썬 챌린지/015.py
@@ -0,0 +1,6 @@
+color=input("Enter your favorite color:")
+
+if color=="red" or color=="RED" or color=="Red":
+    print("I like red too")
+else:
+    print("I dont't like that color, I prefer red")
